1112f32fef871e1d56b9cc8785b2eaaa4a67bbe8
Piyush Nigam <dev.piyushnigam@gmail.com>
2020-06-18 11:54:21 -0700

    formatting the json output

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM node:12.0-slim
 COPY . .
 RUN npm install
+EXPOSE 50000
 CMD [ "node", "server.js", "--port=50000"]
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * @fileoverview Simple http server that dumps out all the incoming request
  * headers as json response.
  */
-var http = require('http'); // 1 - Import Node.js code module
+var http = require('http');  // 1 - Import Node.js code module
 var ArgumentParser = require('argparse').ArgumentParser;
 
 var parser = new ArgumentParser({
@@ -11,27 +11,20 @@ var parser = new ArgumentParser({
   description: 'IAP onprem sample application'
 });
 
-parser.addArgument(
-  [ '--port'],
-  {
-    type: 'int',
-    required: true,
-    help: 'http port that the server listens to'
-  }
-);
+parser.addArgument(['--port'], {
+  type: 'int',
+  required: true,
+  help: 'http port that the server listens to'
+});
 
-var server = http.createServer(function(req,res) { // 2 - creating server
+var server = http.createServer(function(req, res) {  // 2 - creating server
   res.setHeader('Content-Type', 'application/json');
-  console.debug(JSON.stringify({
-    headers: req.headers
-  }));
-  res.end(JSON.stringify({
-    headers: req.headers
-  }));
+  console.debug(JSON.stringify({headers: req.headers}, null, '  '));
+  res.end(JSON.stringify({headers: req.headers}, null, '  '));
 });
 
 var args = parser.parseArgs();
 console.log(args['port']);
-server.listen(args['port']); // 3 - listen for any incoming requests
+server.listen(args['port']);  // 3 - listen for any incoming requests
 
 console.log('Node.js web server at port %s is running..', args['port']);
